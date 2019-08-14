44299f92ed37c2a32ef6b294ee82b51f62a43c82
Drew Gillson <drew.gillson@looker.com>
2019-08-13 21:03:13 -0700

    Added security nodes to all methods

--- proxy.js
+++ proxy.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 var Swagger = require('swagger-client');
+var faunadb = require('faunadb'),
+  q = faunadb.query;
 
 let host = "https://raw.githubusercontent.com/drewgillson/looker-api-swagger-js-client/master/swagger.json"
 
@@ -18,8 +20,17 @@ Swagger(host)
           }
         })
         .then( client => {
-          client.execute({operationId: 'me'}).then(function(response) {
+          client.execute({operationId: 'run_look',
+                          parameters: {look_id: 1,
+                                       result_format: 'json'}}).then(function(response) {
             console.log('Success! ', response)
+
+            var client = new faunadb.Client({
+              secret: process.env.FAUNA_KEY
+            });
+            
+            var createP = client.query(q.Create(q.Collection('run_look'), { data: response }));
+
           })
           .catch(function(error) {
             console.log(error)
